759e4d8b28f2f0899ed7c51ce7fa15366054b827
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2019-07-17 17:00:04 +0100

    Update countries.json with new paths

--- lib/country_data.rb
+++ lib/country_data.rb
@@ -81,7 +81,7 @@ module Everypolitician
       attr_reader :path, :commit_metadata
 
       def remote_source
-        'https://cdn.rawgit.com/loudspeek/representa/%s/%s'
+        'https://raw.githubusercontent.com/loudspeek/representa/%s/%s'
       end
 
       private
